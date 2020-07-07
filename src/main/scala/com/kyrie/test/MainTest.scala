b559de61c1efb00b7904c2307ec1f23e0ac113dc
changyue.li <changyue.li@tendcloud.com>
2020-07-07 11:58:43 +0800

    pom
    Change-Id: Ic798413f7984549c8b762c574e1b29a659f94bbc

---
+++ src/main/scala/com/kyrie/test/MainTest.scala
@@ -0,0 +1,15 @@
+package com.kyrie.test
+
+/**
+ * Created by tend on 2020/5/25.
+ */
+object MainTest {
+
+  def main(args: Array[String]) {
+
+    TestApply().say()
+
+
+  }
+
+}
---
+++ src/main/scala/com/kyrie/test/TestApply.scala
@@ -0,0 +1,25 @@
+package com.kyrie.test
+
+/**
+ * Created by tend on 2020/5/25.
+ */
+class TestApply {
+
+  def say(): Unit ={
+    println("just do it")
+  }
+
+
+}
+
+
+object TestApply {
+
+  def apply():TestApply={
+    println("apply func")
+    new TestApply()
+  }
+
+  println("i am test apply obj")
+
+}
