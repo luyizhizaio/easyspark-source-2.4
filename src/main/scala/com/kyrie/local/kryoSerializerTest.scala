c1df258862af74cc356f2aa8924e122d796b905e
changyue.li <changyue.li@tendcloud.com>
2020-08-04 18:05:25 +0800

    wc
    Change-Id: I6bc4de9b85e3a7167019a6b8cce7b21c210b46a5

--- src/main/scala/com/kyrie/local/kryoSerializerTest.scala
+++ src/main/scala/com/kyrie/local/kryoSerializerTest.scala
@@ -8,9 +8,10 @@ import scala.util.Random
 
 /**
  * Created by tend on 2019/8/15.
+ * kryo与Java对比
  */
 
-object FirstTest {
+object kryoSerializerTest {
 
   case class Test(a: Int = Random.nextInt(1000000),
                   b: Double = Random.nextDouble,
@@ -19,6 +20,12 @@ object FirstTest {
 
   def main(args: Array[String]) {
 
+
+
+  }
+
+
+  def kryoSer(): Unit ={
 
     val sparkConf = new SparkConf()
     sparkConf.set("spark.serializer","org.apache.spark.serializer.KryoSerializer")
@@ -38,6 +45,30 @@ object FirstTest {
 
     val end = System.currentTimeMillis()
     println((end - start)/1000)
+
+  }
+
+
+
+  def javaSer(): Unit ={
+
+    val sparkConf = new SparkConf()
+    val spark = SparkSession.builder().config(sparkConf).master("local").getOrCreate()
+    import spark.implicits._
+
+    val sc = spark.sparkContext
+
+    val start = System.currentTimeMillis()
+
+    val input = sc.parallelize(1 to 100000, 42).map(_ => Test()).toDS.persist(org.apache.spark.storage.StorageLevel.DISK_ONLY)
+    input.count() // Force initialization
+    val shuffled = input.repartition(43).count()
+
+    println(input.rdd.toDebugString)
+
+    val end = System.currentTimeMillis()
+    println((end - start)/1000)
+
   }
 
 
